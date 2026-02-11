4ff1213e634592016ac31756bff3791df06393d6
Clemens Seiler <cseiler@htl-steyr.ac.at>
2026-02-11 14:47:03 +0100

    HelloApplication lädt beim starten als erstes die login.fxml

--- src/main/java/htl/steyr/demo/HelloApplication.java
+++ src/main/java/htl/steyr/demo/HelloApplication.java
@@ -1,19 +1,18 @@
 package htl.steyr.demo;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/htl/steyr/demo/fxml/login.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("EisTrumpf");
         stage.show();
     }
 }
