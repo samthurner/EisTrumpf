eb73292ce36370bb800010e75c1ba60af1cfe594
Clemens Seiler <cseiler@htl-steyr.ac.at>
2026-02-11 14:18:01 +0100

    Bild hinzugefügt und Änderungen in der module-info.java vorgenommen um das Bild in der fxml laden zu können

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,11 @@ module htl.steyr.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens htl.steyr.demo to javafx.fxml;
+    opens htl.steyr.demo.fxml to javafx.fxml;
+    opens image;
+
     exports htl.steyr.demo;
+    exports htl.steyr.demo.controller;
+    opens htl.steyr.demo.controller to javafx.fxml;
 }
