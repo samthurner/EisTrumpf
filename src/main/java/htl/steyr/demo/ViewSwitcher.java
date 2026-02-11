bd2e4b6c70bd28d72a3f5353d464c6b9fd64b0da
samthurner <sthurner@htl-steyr.ac.at>
2026-02-11 14:37:21 +0100

    eine ViewSwitcher klasse zum wechseln zwischen mehreren fxml files erstellt

---
+++ src/main/java/htl/steyr/demo/ViewSwitcher.java
@@ -0,0 +1,26 @@
+package htl.steyr.demo;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class ViewSwitcher {
+
+    private static Stage stage;
+
+    public static void setStage(Stage stage) {
+        ViewSwitcher.stage = stage;
+    }
+
+    public static void switchTo(String fxmlFile) {
+        try {
+            String fxmlFilePath = "/fxml/" + fxmlFile + ".fxml";
+            Parent root = FXMLLoader.load(ViewSwitcher.class.getResource(fxmlFile));
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
