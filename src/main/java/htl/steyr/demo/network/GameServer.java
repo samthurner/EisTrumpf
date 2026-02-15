3d86b3d54b63700c6b61c2d374f1567368ac9c8f
samthurner <sthurner@htl-steyr.ac.at>
2026-02-15 10:38:53 +0100

    Grundriss der Serverklasse

--- src/main/java/htl/steyr/demo/network/GameServer.java
+++ src/main/java/htl/steyr/demo/network/GameServer.java
@@ -1,4 +1,34 @@
 package htl.steyr.demo.network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class GameServer {
+    private int port;
+    private boolean running;
+    private ServerSocket serverSocket;
+    private SocketConnection opponent;
+    private Thread acceptThread;
+
+    public GameServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        running = true;
+
+        serverSocket = new ServerSocket(port);
+
+        acceptThread = new Thread(() -> {
+
+        });
+        acceptThread.start();
+
+    }
+
+    public void stop() throws IOException {
+        running = false;
+        acceptThread.interrupt();
+        serverSocket.close();
+    }
 }
