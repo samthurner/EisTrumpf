b3d0760fb37fd7ad1c41d5d9b7f203a0bd9d184e
Clemens Seiler <cseiler@htl-steyr.ac.at>
2026-02-25 12:07:44 +0100

    Benötigte Veränderungen die für das ändern des Themes und für den Username nötig sind

--- src/main/java/htl/steyr/demo/userdata/UserSession.java
+++ src/main/java/htl/steyr/demo/userdata/UserSession.java
@@ -11,4 +11,29 @@ public class UserSession {
     public static UserData getUserData() {
         return userData;
     }
+
+    private boolean darkMode;
+
+    public void setDarkMode(boolean darkMode) {
+        this.darkMode = darkMode;
+    }
+
+    public boolean isDarkMode() {
+        return darkMode;
+    }
+
+    private static UserSession instance;
+
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setUsername(String newName) {
+        userData.setUsername(newName);
+        userData.writeToJson();
+    }
 }
