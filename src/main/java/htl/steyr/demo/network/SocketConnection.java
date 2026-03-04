2a684bbfea32a5245f4b2ae4f544155278380030
samthurner <sthurner@htl-steyr.ac.at>
2026-03-04 14:31:52 +0100

    Objekte in json umwandeln mittels gson und diese dann verschicken

--- src/main/java/htl/steyr/demo/network/SocketConnection.java
+++ src/main/java/htl/steyr/demo/network/SocketConnection.java
@@ -1,5 +1,7 @@
 package htl.steyr.demo.network;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.IOException;
@@ -11,11 +13,19 @@ public class SocketConnection {
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
+    private Gson gson;
 
     public SocketConnection(Socket socket) throws IOException {
         this.socket = socket;
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
+        this.gson = new Gson();
+    }
+
+    // Verschickt elemente als Json objekt
+    public void sendMessage(Object obj) {
+        String Json = gson.toJson(obj);
+        out.println(Json);
     }
 
     public void send(String message) {
