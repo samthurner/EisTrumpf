336542ac7fc8d89184d43d7bfd0e3db08770b608
Clemens Seiler <cseiler@htl-steyr.ac.at>
2026-03-04 12:10:19 +0100

    Veränderungen am Code,dass beim Namen ändern in den Einstellungen die Ursprüngliche Json datei vom Namen davor gelöscht wird .

--- src/main/java/htl/steyr/demo/controller/NameAlertController.java
+++ src/main/java/htl/steyr/demo/controller/NameAlertController.java
@@ -1,9 +1,13 @@
 package htl.steyr.demo.controller;
 
 import htl.steyr.demo.ViewSwitcher;
+import htl.steyr.demo.userdata.UserData;
 import htl.steyr.demo.userdata.UserSession;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.nio.file.Files;
 
 public class NameAlertController {
 
@@ -15,8 +19,17 @@ public class NameAlertController {
         String newName = nameField.getText();
 
         if (newName != null && !newName.isBlank()) {
-            UserSession.getInstance().getUserData().setName(newName);
-            System.out.println("Neuer Name: " + newName);
+            UserData user = UserSession.getInstance().getUserData();
+            String oldName = user.getUsername();
+            user.setUsername(newName);
+            user.writeToJson();
+            File directory = new File("Json");
+            for(File file : directory.listFiles()){
+                if(file.getName().equals(oldName + ".json")){
+                    file.delete();
+                }
+            }
+
         }
 
         ViewSwitcher.switchTo("settings-pane");
