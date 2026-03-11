a5e20a536ed45a0410b9124af224b524e80bb3f8
samthurner <sthurner@htl-steyr.ac.at>
2026-03-11 13:50:02 +0100

    fullscreen

--- src/main/java/htl/steyr/demo/GameApplication.java
+++ src/main/java/htl/steyr/demo/GameApplication.java
@@ -4,9 +4,12 @@ package htl.steyr.demo;
 import htl.steyr.demo.music.Music;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class GameApplication extends Application {
     @Override
@@ -20,6 +23,16 @@ public class GameApplication extends Application {
 
         stage.setScene(new Scene(root));
         stage.setTitle("EisTrumpf");
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+
+        stage.show();
 
         stage.show();
     }
