f1090f67243aa83044a37efbb7a9446f79a5aa27
Fabian <fschmidb@htl-steyr.ac.at>
2026-03-29 15:09:21 +0200

    bug fixed

--- src/main/java/htl/steyr/demo/controller/LoginController.java
+++ src/main/java/htl/steyr/demo/controller/LoginController.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.sql.SQLOutput;
 
